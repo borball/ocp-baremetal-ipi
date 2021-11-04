67d7542ac2202410842bea942c4a66b6cf914cb1
Borball Zhai <borball.zh@gmail.com>
2021-11-04 13:20:47 -0400

    update

--- helper_node/mirror-olm-catalog.sh
+++ helper_node/mirror-olm-catalog.sh
@@ -10,13 +10,13 @@ export REDHAT_DEFAULT_REGISTRY=registry.redhat.io
 export OCP_RELEASE_SHORT=${OCP_RELEASE%\.*}
 
 declare -A remote_index
-remote_index["certified_operator"]="${REDHAT_DEFAULT_REGISTRY}/redhat/certified-operator-index:v${OCP_RELEASE_SHORT}"
 remote_index["redhat_operator"]="${REDHAT_DEFAULT_REGISTRY}/redhat/redhat-operator-index:v${OCP_RELEASE_SHORT}"
+remote_index["certified_operator"]="${REDHAT_DEFAULT_REGISTRY}/redhat/certified-operator-index:v${OCP_RELEASE_SHORT}"
 remote_index["community_operator"]="${REDHAT_DEFAULT_REGISTRY}/redhat/community-operator-index:v${OCP_RELEASE_SHORT}"
 
 declare -A local_index
-local_index["certified_operator"]="${LOCAL_REGISTRY}/olm-index/certified-operator-index:v${OCP_RELEASE_SHORT}"
 local_index["redhat_operator"]="${LOCAL_REGISTRY}/olm-index/redhat-operator-index:v${OCP_RELEASE_SHORT}"
+local_index["certified_operator"]="${LOCAL_REGISTRY}/olm-index/certified-operator-index:v${OCP_RELEASE_SHORT}"
 local_index["community_operator"]="${LOCAL_REGISTRY}/olm-index/community-operator-index:v${OCP_RELEASE_SHORT}"
 
 declare -A packages
@@ -24,9 +24,35 @@ packages["redhat_operator"]="local-storage-operator,ocs-operator,performance-add
 packages["certified_operator"]="sriov-fec,n3000"
 packages["community_operator"]="hive-operator"
 
-
 # Disable all default CatalogSource
 oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
+
+# redhat-operator
+redhat_operator(){
+  # build index
+  opm index prune --from-index ${remote_index[redhat_operator]} --packages ${packages[redhat_operator]} --tag ${local_index[redhat_operator]} --permissive
+  GODEBUG=x509ignoreCN=0 podman push ${local_index[redhat_operator]}
+  # mirror images
+  GODEBUG=x509ignoreCN=0 oc adm catalog mirror ${local_index[redhat_operator]} ${LOCAL_REGISTRY_OLM} -a ${REGISTRY_AUTH_FILE} --max-components=5 --to-manifests=redhat-operator-index/
+
+  # Create CatalogSource
+  oc apply -f - <<EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: redhat-operators
+  namespace: openshift-marketplace
+spec:
+  displayName: Red Hat Operators Mirror
+  image: ${local_index[redhat_operator]}
+  publisher: Red Hat
+  sourceType: grpc
+EOF
+
+  if [ -s redhat-operator-index/imageContentSourcePolicy.yaml ]; then
+    oc apply -f redhat-operator-index/imageContentSourcePolicy.yaml
+  fi
+}
 
 # certified-operator
 certified_operator(){
@@ -56,33 +82,6 @@ EOF
 
 }
 
-# redhat-operator
-redhat_operator(){
-  # build index
-  opm index prune --from-index ${remote_index[redhat_operator]} --packages ${packages[redhat_operator]} --tag ${local_index[redhat_operator]} --permissive
-  GODEBUG=x509ignoreCN=0 podman push ${local_index[redhat_operator]}
-  # mirror images
-  GODEBUG=x509ignoreCN=0 oc adm catalog mirror ${local_index[redhat_operator]} ${LOCAL_REGISTRY_OLM} -a ${REGISTRY_AUTH_FILE} --max-components=5 --to-manifests=redhat-operator-index/
-
-  # Create CatalogSource
-  oc apply -f - <<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: CatalogSource
-metadata:
-  name: redhat-operators
-  namespace: openshift-marketplace
-spec:
-  displayName: Red Hat Operators Mirror
-  image: ${local_index[redhat_operator]}
-  publisher: Red Hat
-  sourceType: grpc
-EOF
-
-  if [ -s redhat-operator-index/imageContentSourcePolicy.yaml ]; then
-    oc apply -f redhat-operator-index/imageContentSourcePolicy.yaml
-  fi
-}
-
 # community-operator
 community_operator(){
   # build index
@@ -110,6 +109,6 @@ EOF
   fi
 }
 
-certified_operator
 redhat_operator
+#certified_operator
 #community_operator
