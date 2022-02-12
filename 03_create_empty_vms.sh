afc5c6351e6ccc3cfac75e1736b0a5cb5f0ce7c0
Borball Zhai <borball.zh@gmail.com>
2022-02-12 12:28:58 -0500

    add 3rd disk

--- 03_create_empty_vms.sh
+++ 03_create_empty_vms.sh
@@ -3,7 +3,7 @@
 CLUSTER=${cluster_name}
 VIRT_NIC=${network_name}
 
-kcli create vm -P start=False -P memory=32000 -P numcpus=16 -P disks=[100,200] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"ens3\",\"mac\":\"de:ad:be:ff:00:05\"}"] ${CLUSTER}-master0
-kcli create vm -P start=False -P memory=32000 -P numcpus=16 -P disks=[100,200] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"ens3\",\"mac\":\"de:ad:be:ff:00:06\"}"] ${CLUSTER}-master1
-kcli create vm -P start=False -P memory=32000 -P numcpus=16 -P disks=[100,200] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"ens3\",\"mac\":\"de:ad:be:ff:00:07\"}"] ${CLUSTER}-master2
+kcli create vm -P start=False -P memory=32000 -P numcpus=16 -P disks=[100,200,200] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"ens3\",\"mac\":\"de:ad:be:ff:00:05\"}"] ${CLUSTER}-master0
+kcli create vm -P start=False -P memory=32000 -P numcpus=16 -P disks=[100,200,200] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"ens3\",\"mac\":\"de:ad:be:ff:00:06\"}"] ${CLUSTER}-master1
+kcli create vm -P start=False -P memory=32000 -P numcpus=16 -P disks=[100,200,200] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"ens3\",\"mac\":\"de:ad:be:ff:00:07\"}"] ${CLUSTER}-master2
 
