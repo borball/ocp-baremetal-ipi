c48a0b199d89f7df2264436ef3c91c6c672e04e3
Borball Zhai <borball.zh@gmail.com>
2022-04-13 15:16:40 -0400

    Update 02_create_helper_vm.sh

--- 02_create_helper_vm.sh
+++ 02_create_helper_vm.sh
@@ -3,4 +3,4 @@
 CLUSTER=${cluster_name}
 VIRT_NIC=${network_name}
 
-kcli create vm -i centos8 -P start=True -P memory=8000 -P numcpus=2 -P disks=[300] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:00:00\",\"ip\":\"192.168.10.100\",\"mask\":\"255.255.255.0\",\"dns\":\"${dns_server}\",\"gateway\":\"192.168.10.1\"}"] ${CLUSTER}-helper
+kcli create vm -i centos8stream -P start=True -P memory=8000 -P numcpus=2 -P disks=[300] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:00:00\",\"ip\":\"192.168.10.100\",\"mask\":\"255.255.255.0\",\"dns\":\"${dns_server}\",\"gateway\":\"192.168.10.1\"}"] ${CLUSTER}-helper
