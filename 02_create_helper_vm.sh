980640415dd571aa29b580b734f8cc51e4269cc3
Borball Zhai <borball.zh@gmail.com>
2022-05-18 17:18:45 -0400

    init add for bos2 dual stack

--- 02_create_helper_vm.sh
+++ 02_create_helper_vm.sh
@@ -3,4 +3,4 @@
 CLUSTER=${cluster_name}
 VIRT_NIC=${network_name}
 
-kcli create vm -i centos8 -P start=True -P memory=8000 -P numcpus=2 -P disks=[200] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"ens3\",\"mac\":\"de:ad:be:ff:00:01\"}"] ${CLUSTER}-helper
+kcli create vm -i centos8stream -P start=True -P memory=8000 -P numcpus=2 -P disks=[200] -P nets=["{\"name\":\"${VIRT_NIC}\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:00:01\"}"] -P dualstack=true ${CLUSTER}-helper
